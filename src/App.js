1375dfdcec6ba5773c5448ebac76d49956141d99
Abood10s <saafeenabood@gmail.com>
2023-08-10 10:32:54 +0300

    .env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 function App() {
-  return <div className="App">test</div>;
+  console.log(process.env.REACT_APP_YOUR_API_KEY);
+  return <div className="App">test :{process.env.REACT_APP_YOUR_API_KEY}</div>;
 }
 
 export default App;
