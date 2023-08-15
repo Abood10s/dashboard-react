87b5293cd906bf4f6929a18ff0cd2ab191bbd969
Abood10s <saafeenabood@gmail.com>
2023-08-15 16:08:30 +0300

    adding SVG's

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import ProductsProvider from "./Context";
 import Layout from "./components/Layout";
 import UploadForm from "./components/UploadForm";
-
+import { DollarSvg } from "./assets/dollar";
+import { PeopleSvg } from "./assets/people";
+import ProgressSvg from "./assets/progress";
 function App() {
   console.log(process.env.REACT_APP_YOUR_API_KEY);
   return (
     <div className="App">
       <ProductsProvider>
         <Layout>
+          <DollarSvg />
+
+          <DollarSvg color="rgb(99, 102, 241)" />
+
+          <PeopleSvg />
+          <ProgressSvg />
           <UploadForm />
           <UploadForm />
           <UploadForm />
---
+++ src/components/Stats-card/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+const StatsCard = () => {
+  return <div>StatsCard</div>;
+};
+
+export default StatsCard;
