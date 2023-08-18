733e9d2c8d7b0bdfb3008b0f36bbfc81a676f9e4
Abood10s <saafeenabood@gmail.com>
2023-08-18 17:05:54 +0300

    adding search section

---
+++ src/ProductEditComponent/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UploadForm from "../components/UploadForm/index";
+import "./style.css";
+const ProductEdit = () => {
+  return (
+    <div>
+      <UploadForm />
+    </div>
+  );
+};
+
+export default ProductEdit;
